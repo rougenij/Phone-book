e208fde71fc142538021eda645fc657d8230ba01
Claude Ni <Claudenij2@gmail.com>
2024-07-10 20:38:21 +0300

    added official starter data

---
+++ js/index.js
@@ -0,0 +1,56 @@
+const data = [
+  {
+    img: "../assets/Dan.jpg",
+    name: "Dan Salvatori",
+    number: "0597684218",
+    address: "Haifa",
+  },
+  {
+    img: "../assets/Karen.jpg",
+    name: "Karen Booma",
+    number: "0521544362",
+    address: "Tel Aviv",
+  },
+  {
+    img: "../assets/Lara.jpg",
+    name: "Lara Lorance",
+    number: "0559521234",
+    address: "Haifa",
+  },
+  {
+    img: "../assets/Mike.jpg",
+    name: "Mike Tomi",
+    number: "0596549874",
+    address: "Eilat",
+  },
+];
+const list = document.querySelector(".list");
+
+data.forEach((elem) => addUser(elem));
+
+function addUser(user) {
+  const li = document.createElement("li");
+  li.classList = "flex item";
+  li.innerHTML = `
+    <div class="flex">
+        <img
+        class="pfp"
+        src="${user.img}"
+        alt="${user.name} image"
+        />
+        <p class="name">${user.name}</p>
+    </div>
+    <div class="flex">
+        <button class="icon-btn">
+        <i class="fa-solid fa-circle-info"></i>
+        </button>
+        <button class="icon-btn">
+        <i class="fa-solid fa-user-pen"></i>
+        </button>
+        <button class="icon-btn">
+        <i class="fa-solid fa-user-minus"></i>
+        </button>
+    </div>
+  `;
+  list.append(li);
+}
